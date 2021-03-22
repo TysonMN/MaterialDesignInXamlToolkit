635a110ee5e203e63e00ff0191fa6c75c57dc400
Tyson Williams <tyson.d.w@gmail.com>
2021-03-22 08:54:25 -0500

    code clean up

--- MaterialDesignThemes.Wpf/DialogHost.cs
+++ MaterialDesignThemes.Wpf/DialogHost.cs
@@ -2,7 +2,6 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
-using System.Windows.Data;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Threading;
